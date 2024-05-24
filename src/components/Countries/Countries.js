28acda9b762bb2bcf134c1af65fe8499d64d1daa
absujan <abdussabursujan@gmail.com>
2024-05-24 21:30:45 +0600

    body

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -12,9 +13,11 @@ const Countries = () => {
         <div>
          <h2>Welcome to Countries Information. site 
          Total Country: {countries.length}</h2>
+         <div className='grid-div'>
          {
-            countries.map(country => <Country name={country.name.common}></Country>)
+            countries.map(country => <Country country={country}></Country>)
          }
+         </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Country.css'
 
 const Country = (props) => {
+    const {name, flags, capital, population, region,} = props.country;
+    
     return (
-        <div>
-            <h2> Country Name: {props.name}</h2>
+        <div className='child-box'>
+            <img className='flagImage' src={flags.png} alt="" />
+           <div className='information'>
+            <div>
+            <h4 className='h4Tags'> Country Name: {name.common} </h4>
+            <h4>Capital: {capital}</h4>
+            </div>
+            <div>
+            <h4>Population: {population}</h4>
+            <h4>Region: {region}</h4>
+            </div>
+           </div>
         </div>
     );
 };
